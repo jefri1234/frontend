60b3ef4e971996aa301f5712bf4306f7cc94c9c6
max <obregonjefferson2@gmail.com>
2024-09-28 22:37:02 -0500

    proyecto login

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
+//componentes
+import App from './App.jsx'
+import Welcome from './welcome.jsx'
+
+
+const jeff= createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>
+  },
+  {
+    path:"welcome",
+    element:<Welcome/>
+  }
+
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={jeff}/>
   </React.StrictMode>,
 )
---
+++ src/welcome.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+
+function Welcome() {
+  return (
+    <div>
+      
+        <h1>WELCOME TO HOME</h1>
+        <Link to={"/"}>
+            <button>volver</button>
+        </Link>
+        <h1>esot es hotm</h1>
+        <h1>titulo#</h1>
+        <h1>titulo#</h1>
+        <h1>titulo#</h1>
+        <h1>titulo#</h1>
+        <h1>titulo#</h1>
+        <h1>uno mas jaja</h1>
+    
+    </div>
+  )
+}
+
+export default Welcome
